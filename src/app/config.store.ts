6aa0848c676b853e7e68a29b19f0d445a4c963f5
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2017-07-11 17:22:16 -0500

    first template

--- src/app/config.store.ts
+++ src/app/config.store.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/publishReplay';
 
 import { ValWrapper } from './val-wrapper';
 
