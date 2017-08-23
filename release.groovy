b9d73af9690546fca53698a6adc4330be06f3023
Carlos Feria <carlosthe19916@gmail.com>
2017-08-23 16:07:45 -0500

    changes

--- release.groovy
+++ release.groovy
@@ -30,7 +30,7 @@ def cd (b){
     }
 
     stage('release'){
-        def published = npmReleaseOpenfact{
+        def published = npmRelease{
             branch = b
         }
         return published
@@ -47,46 +47,6 @@ def updateDownstreamProjects(v){
         version = v
         containerName = 'ui'
         autoMerge = true
-    }
-}
-
-def npmReleaseOpenfact(body) {
-    // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-
-    def gitEmail = config.gitEmail ?: 'carlosthe19916@gmail.com'
-    def gitUserName = config.gitUserName ?: 'carlosthe19916'
-    def branch = config.branch
-
-    sh "git config user.email ${gitEmail}"
-    sh "git config user.name ${gitUserName}"
-
-    //sh 'chmod 600 /root/.ssh-git/ssh-key'
-    //sh 'chmod 600 /root/.ssh-git/ssh-key.pub'
-    //sh 'chmod 700 /root/.ssh-git'
-
-    String npmToken = readFile '/home/jenkins/.npm-token/token'
-    String ghToken = readFile '/home/jenkins/.apitoken/hub'
-    wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [
-        [password: npmToken, var: 'NPM_PASSWORD'],
-        [password: ghToken, var: 'GH_PASSWORD']]]) {
-
-        try {
-            sh """
-            export NPM_TOKEN=${npmToken} 
-            export GITHUB_TOKEN=${ghToken}
-            export GIT_BRANCH=${branch}
-            npm run semantic-release
-            """
-        } catch (err) {
-            echo "ERROR publishing: ${err}"
-            echo "No artifacts published so skip updating downstream projects"
-            return false
-        }
-        return true
     }
 }
 
