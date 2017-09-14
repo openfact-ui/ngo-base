3d6245d1c5a0321ba19106457607f260ac35dc29
carlosthe19916 <carlosthe19916@gmail.com>
2017-09-14 11:16:36 -0500

    fix(notification): add notifications

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
+// Notifications
+export { Notification } from './src/app/notifications/notification';
+export { NotificationAction } from './src/app/notifications/notification-action';
+export { Notifications } from './src/app/notifications/notifications';
+export { NotificationType } from './src/app/notifications/notification-type';
+
 // Broadcaster
 export { Broadcaster } from './src/app/broadcaster.service';
 
